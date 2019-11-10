7828405bb174e8f9f5062a575ad887f03ecbf8bc
Matthew-SA <surfsnow1@gmail.com>
2019-11-10 10:28:35 -0800

    complete (O)n two_sum

---
+++ 1.two_sum.rb
@@ -0,0 +1,11 @@
+def two_sum(nums, target)
+    seen = Hash.new
+    nums.each_with_index { |num, i| seen[num] = i }
+    p seen
+    nums.each_with_index do |num, i|
+        req_num = target - num
+        if seen.has_key?(req_num) && i != seen[req_num]
+            return [i, seen[req_num]]
+        end
+    end
+end
