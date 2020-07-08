fb8d86fd2071768e1c8e0cfea42db01bdd1b1f71
Matthew-SA <surfsnow1@gmail.com>
2020-07-08 10:36:32 -0700

    add july challenge day 7 island perimeter

---
+++ challenge_7_2020/island_perimeter.js
@@ -0,0 +1,62 @@
+// You are given a map in form of a two-dimensional integer grid where 1 
+// represents land and 0 represents water.
+
+// Grid cells are connected horizontally/vertically (not diagonally). The grid 
+// is completely surrounded by water, and there is exactly one island (i.e., one 
+// or more connected land cells).
+
+// The island doesn't have "lakes" (water inside that isn't connected to the 
+// water around the island). One cell is a square with side length 1. The grid 
+// is rectangular, width and height don't exceed 100. Determine the perimeter 
+// of the island.
+
+ 
+
+// Example:
+
+// Input:
+// [[0,1,0,0],
+//  [1,1,1,0],
+//  [0,1,0,0],
+//  [1,1,0,0]]
+
+// Output: 16
+
+// Explanation: The perimeter is the 16 yellow stripes in the image below:
+
+
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+const islandPerimeter = (grid) => {
+  let isHorizLand = false;
+  let isVertLand = false;
+  let count = 0;
+  let length = grid.length > grid[0].length ? grid.length : grid[0].length;
+
+  for (let i = 0; i < length; i++) {
+    isHorizLand = false;
+    isVertLand = false;
+
+    for (let j = 0; j < length; j++) {
+      let horizTile = grid[i]?.[j];
+      let vertTile = grid[j]?.[i];
+
+      if ((!isHorizLand && horizTile) || (isHorizLand && !horizTile)) {
+        isHorizLand = !isHorizLand;
+        count++;
+      }
+
+      if ((!isVertLand && vertTile) || (isVertLand && !vertTile)) {
+        isVertLand = !isVertLand;
+        count++;
+      }
+    }
+    if (isHorizLand) count++;
+    if (isVertLand) count++;
+  }
+
+  return count;
+};
