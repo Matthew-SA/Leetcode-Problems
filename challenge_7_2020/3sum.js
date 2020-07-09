b44c920814429a98048fea96b3d2a52780e4f30c
Matthew-SA <surfsnow1@gmail.com>
2020-07-08 17:42:22 -0700

    complete july challenge day 8 3Sum

--- challenge_7_2020/3sum.js
+++ challenge_7_2020/3sum.js
@@ -1,5 +1,59 @@
+// Given an array nums of n integers, are there elements a, b, c in nums such that 
+// a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+// Note:
+
+// The solution set must not contain duplicate triplets.
+
+// Example:
+
+// Given array nums = [-1, 0, 1, 2, -1, -4],
+
+// A solution set is:
+// [
+//   [-1, 0, 1],
+//   [-1, -1, 2]
+// ]
 
 
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+const threeSum = nums => {
+  let sortedNums = nums.sort((a,b) => a - b);
+  let results = [];
+
+  let lastOuterNum = undefined;
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (lastOuterNum === sortedNums[i]) continue;
+    lastOuterNum = sortedNums[i];
+    
+    let lastInnerNum = undefined;
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      if (lastInnerNum === sortedNums[j]) continue;
+      lastInnerNum = sortedNums[j];
+
+      let difference = -(sortedNums[i] + sortedNums[j]);
+      let search = bSearch(sortedNums, j+1, nums.length, difference);
+      if (search > 1) results.push([sortedNums[i], sortedNums[j], difference]);
+    };
+  };
+  return results;
+};
+
+const bSearch = (arr, start, end, target) => {
+  if (start > end) return -1;
+  let pivot = Math.floor((start + end) / 2);
+  
+  if (arr[pivot] === target) {
+    return pivot;
+  } else if (target < arr[pivot]) {
+    return bSearch(arr, start, pivot - 1, target);
+  } else {
+    return bSearch(arr, pivot + 1, end, target);
+  }
+};
 
 
 
