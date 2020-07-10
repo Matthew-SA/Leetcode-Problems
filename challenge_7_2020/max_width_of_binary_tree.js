db3ae725c644cb98adf2f9d9e889845705e7d6d1
Matthew-SA <surfsnow1@gmail.com>
2020-07-09 23:36:51 -0700

    add copied solution for max width of binary tree

---
+++ challenge_7_2020/max_width_of_binary_tree.js
@@ -0,0 +1,109 @@
+// Given a binary tree, write a function to get the maximum width of the given 
+// tree.The width of a tree is the maximum width among all levels.The binary 
+// tree has the same structure as a full binary tree, but some nodes are null.
+
+// The width of one level is defined as the length between the end - 
+// nodes(the leftmost and right most non - null nodes in the level, where the 
+// null nodes between the end - nodes are also counted into the length calculation.
+
+//   Example 1:
+
+//   Input:
+
+//   1
+//   /   \
+//   3     2
+// / \     \
+//   5   3     9 
+
+// Output: 4
+// Explanation: The maximum width existing in the third level with the length 4(5, 3, null, 9).
+//   Example 2:
+
+// Input:
+
+// 1
+//   /
+//   3
+//   / \
+// 5   3
+
+// Output: 2
+// Explanation: The maximum width existing in the third level with the length 2(5, 3).
+//   Example 3:
+
+// Input:
+
+// 1
+//   / \
+// 3   2
+//   /
+//   5
+
+// Output: 2
+// Explanation: The maximum width existing in the second level with the length 2(3, 2).
+//   Example 4:
+
+// Input:
+
+// 1
+//   / \
+// 3   2
+//   /     \
+// 5       9
+//   /         \
+// 6           7
+// Output: 8
+// Explanation: The maximum width existing in the fourth level with the length 8(6, null, null, null, null, null, null, 7).
+
+
+//   Note: Answer will in the range of 32 - bit signed integer.
+
+
+**************************************
+// Borrowed solution ->
+// Review this solution for future use**
+**************************************
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var widthOfBinaryTree = function (root) {
+  if (!root) return 0;
+
+  let maxWidth = 0;
+  const queue = [];
+  queue.push([root, 0]);
+
+  while (queue.length > 0) {
+
+    const len = queue.length;
+    let node, pos, first;
+
+    for (let i = 0; i < len; i++) {
+      [node, pos] = queue.shift();
+
+      if (i === 0) first = pos;
+      if (node.left) queue.push([node.left, pos * 2]);
+      if (node.right) queue.push([node.right, (pos * 2) + 1]);
+    }
+
+    const currWidth = (pos - first + 1) | 0;
+    maxWidth = Math.max(maxWidth, currWidth);
+  }
+
+  return maxWidth;
+};
